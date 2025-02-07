feb796de8a5d9d34275e460ab2f99f89bc4a689f
Mahi0211 <mahinpranave@gmail.com>
2025-02-07 23:18:15 +0530

    authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,67 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { motion } from "framer-motion";
+import { checkValidateData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase"; //it's coming from a central place
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errMessage, setErrMessage] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    const message = checkValidateData(
+      name.current.value,
+      email.current.value,
+      password.current.value
+    );
+    setErrMessage(message);
+    if (message) return;
+
+    // sign in and sign up logic
+    if (!isSignInForm) {
+      // sign up logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      // sign in logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrMessage(errorCode + "-" + errorMessage);
+        });
+    }
+  };
 
   const toogleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
@@ -13,12 +72,15 @@ const Login = () => {
       <Header />
       <img
         src="https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_large.jpg"
-        srcset="https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_large.jpg 2000w, https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_medium.jpg 1279w, https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_small.jpg 959w"
-        alt=""
-        aria-hidden="true"
+        alt="logo"
         className="absolute"
       />
-      <form
+      <motion.form
+        onSubmit={(e) => e.preventDefault()}
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.9 }}
+        transition={{ duration: 0.4, ease: "easeInOut" }}
         className="absolute w-[30%] mx-auto mt-[92px] right-0 left-0
        bg-black text-white flex flex-col px-[68px] py-[48px] bg-opacity-80 rounded-md"
       >
@@ -28,22 +90,31 @@ const Login = () => {
         <div className="flex flex-col gap-4 items-center">
           {!isSignInForm && (
             <input
+              ref={name}
               type="text"
               placeholder="Name"
               className="p-4 rounded-[4px] w-full bg-black bg-opacity-60 ring-1 ring-[#5E5F5F]"
             />
           )}
           <input
+            ref={email}
             type="text"
             placeholder="Email or mobile number"
             className="p-4 rounded-[4px] w-full bg-black bg-opacity-60 ring-1 ring-[#5E5F5F]"
           />
           <input
+            ref={password}
             type="Password"
             placeholder="Password"
             className="p-4 rounded-[4px] w-full bg-black bg-opacity-60 ring-1 ring-[#5E5F5F]"
           />
-          <button className="px-4 py-[6px] bg-[#C30702] rounded-[4px] w-full">
+          <p className="text-center text-[#E53935] font-semibold">
+            {errMessage}
+          </p>
+          <button
+            className="px-4 py-[6px] bg-[#C30702] rounded-[4px] w-full"
+            onClick={handleButtonClick}
+          >
             {isSignInForm ? "Sign in" : "Sign up"}
           </button>
           {isSignInForm && (
@@ -77,7 +148,7 @@ const Login = () => {
             {isSignInForm ? "Sign up now" : "Sign in now"}
           </span>
         </p>
-      </form>
+      </motion.form>
     </div>
   );
 };
