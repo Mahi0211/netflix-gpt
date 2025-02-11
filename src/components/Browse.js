a18597dfe0e69687925d4962667844b8d9e76fd5
Mahi0211 <mahinpranave@gmail.com>
2025-02-11 20:19:33 +0530

    creating a custom hook useNowPlayingMovies for cleanig Browse component(neat)

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,25 +1,9 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../redux/moviesSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-
-  const getNowPlayingMovies = async () => {
-    const res = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const data = await res.json();
-    console.log(data.results);
-    dispatch(addNowPlayingMovies(data.results));
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
 
   return (
     <div>
