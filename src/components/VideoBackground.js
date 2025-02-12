8b12eef9099f77f5a82a43a62ed2787d002fd5c5
Mahi0211 <mahinpranave@gmail.com>
2025-02-13 00:07:16 +0530

    custom hook for getMovieTrailer

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,27 +1,10 @@
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addMovieTrailer } from "../redux/moviesSlice";
+import { useSelector } from "react-redux";
+import getMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-  const dispatch = useDispatch();
+  getMovieTrailer({ movieId });
+
   const trailerKey = useSelector((state) => state.movies?.movieTrailer);
-
-  const getMovieTrailer = async () => {
-    const res = await fetch(
-      "https://api.themoviedb.org/3/movie/" + movieId + "/videos",
-      API_OPTIONS
-    );
-    const data = await res.json();
-
-    const filterData = data.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : data.results[0];
-    dispatch(addMovieTrailer(trailer.key));
-  };
-
-  useEffect(() => {
-    getMovieTrailer();
-  }, []);
 
   return (
     <div>
--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,14 +1,22 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addMovieTrailer } from "../redux/moviesSlice";
 
-const useMovieTrailer = () => {
+const useMovieTrailer = ({ movieId }) => {
+  const dispatch = useDispatch();
+
   const getMovieTrailer = async () => {
     const res = await fetch(
-      "https://api.themoviedb.org/3/movie/939243/videos",
+      "https://api.themoviedb.org/3/movie/" + movieId + "/videos",
       API_OPTIONS
     );
     const data = await res.json();
-    console.log(data);
+
+    const filterData = data.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : data.results[0];
+    
+    dispatch(addMovieTrailer(trailer.key));
   };
 
   useEffect(() => {
