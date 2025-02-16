34823446eb3952f55613fc55b52d047eec68485f
Mahi0211 <mahinpranave@gmail.com>
2025-02-17 00:28:43 +0530

    constants recreated

---
+++ Frontend/src/utils/constants.js
@@ -0,0 +1,33 @@
+export const BG_IMG =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_large.jpg";
+
+export const USER_AVATAR = "https://example.com/jane-q-user/profile.jpg";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZTA3N2I4NDYzYmQ2OTliMDc5YzAyMDVjOTkxNzdlMSIsIm5iZiI6MTczOTE5MTA3MS43Mjg5OTk5LCJzdWIiOiI2N2E5ZjMxZmIwMTE1M2Q2ZjQ5MzdjYjciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.lj7FkXvsIQJz6ez3E1QyKXo75MLyXhKD8gHFzNsJJxw",
+  },
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
+
+export const LANG_CHANGE = [
+  {
+    identifier: "English",
+  },
+  {
+    identifier: "Hindi",
+  },
+  {
+    identifier: "Tamil",
+  },
+  {
+    identifier: "Kannada",
+  },
+  {
+    identifier: "Spanish",
+  },
+];
